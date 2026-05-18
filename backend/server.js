448d91d400d7fd7c53bc7b074c88e017e2fdc5df
Agamjot Singh <agamjotsingh1801@gmail.com>
2026-05-18 15:35:51 +0530

    commit added

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://printfrall.vercel.app'],
+  credentials: true
+}));
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
